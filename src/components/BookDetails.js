f5cc1ac2c041762d37ec697831747f60e43f268a
morteza khaki <mortezakhaki@pursuit.org>
2020-09-19 04:11:06 -0400

    reducer added to the app

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -2,14 +2,30 @@ import React, { useContext } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const BookDetails = ({ book }) => {
-    const { removeBook } = useContext(BookContext)
-    return ( 
-        <li onClick={() => removeBook(book.id)}>
-            <div className='title'> {book.title} </div>
-            <div className='author'> {book.author} </div>
-        </li>
-     );
+  const { dispatch } = useContext(BookContext);
+  return (
+    <li onClick={() => dispatch({ type: 'REMOVE_BOOK', id: book.id })}>
+      <div className="title">{book.title}</div>
+      <div className="author">{book.author}</div>
+    </li>
+  );
 }
- 
+
 export default BookDetails;
+
+
+// import React, { useContext } from 'react';
+// import { BookContext } from '../contexts/BookContext';
+
+// const BookDetails = ({ book }) => {
+//     const { removeBook } = useContext(BookContext)
+//     return ( 
+//         <li onClick={() => removeBook(book.id)}>
+//             <div className='title'> {book.title} </div>
+//             <div className='author'> {book.author} </div>
+//         </li>
+//      );
+// }
+ 
+// export default BookDetails;
 
